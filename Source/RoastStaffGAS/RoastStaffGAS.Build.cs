1c545f25131314d71f1ba30113e24cba7bfd1ffc
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-13 15:28:49 +0900

    <feat> project initialize

---
+++ Source/RoastStaffGAS/RoastStaffGAS.Build.cs
@@ -0,0 +1,52 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RoastStaffGAS : ModuleRules
+{
+	public RoastStaffGAS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks",
+			
+			"Niagara",
+			
+			"UMG",
+			"Slate",
+			"SlateCore"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"RoastStaffGAS",
+			"RoastStaffGAS/Variant_TwinStick",
+			"RoastStaffGAS/Variant_TwinStick/AI",
+			"RoastStaffGAS/Variant_TwinStick/Gameplay",
+			"RoastStaffGAS/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/RoastStaffGAS/RoastStaffGASGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RoastStaffGASGameMode.h"
+
+ARoastStaffGASGameMode::ARoastStaffGASGameMode()
+{
+	// stub
+}
