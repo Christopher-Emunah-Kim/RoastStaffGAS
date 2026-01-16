d118059a5a0fbdb2a2f907df6b8ec2bb28326105
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-16 11:44:32 +0900

    <feat> Enemy Character 생성

---
+++ Source/RoastStaffGAS/Public/Character/K_EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/K_BaseCharacter.h"
+#include "K_EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROASTSTAFFGAS_API AK_EnemyCharacter : public AK_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
