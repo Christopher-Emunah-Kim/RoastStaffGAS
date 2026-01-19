37453ba6a22a4e945da167cb130eadcd3d2a78c3
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-19 17:31:03 +0900

    <feat> Net 과제용 픽업 아이템 추가

---
+++ Source/RoastStaffGAS/Private/Temp/Net_PickupItem.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Temp/Net_PickupItem.h"
+
+#include "NavigationSystemTypes.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "System/K_LoggingSystem.h"
+#include "Temp/Net_PlayerCharacter.h"
+
+ANet_PickupItem::ANet_PickupItem()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	bReplicates = true;
+	
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+	
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComp"));
+	CollisionComp->SetupAttachment(RootComponent);
+	CollisionComp->SetSphereRadius(100.f);
+	CollisionComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionComp->SetCollisionObjectType(ECC_WorldDynamic);
+	CollisionComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	StaticMeshComp->SetupAttachment(CollisionComp);
+	StaticMeshComp->SetCollisionProfileName(FName("NoCollision"));
+}
+
+
+void ANet_PickupItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ANet_PickupItem::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	//서버에서만 처리
+	if (GetLocalRole() != ROLE_Authority)
+	{
+		return;
+	}
+	
+	ANet_PlayerCharacter* player = Cast<ANet_PlayerCharacter>(OtherActor);
+	if (!ensureMsgf(player, TEXT("Overlapped Actor is not player")))
+	{
+		return;
+	}
+	player->AddPickUp();
+	
+	KHS_INFO(TEXT("[Net_PickupItem] Picked up by %s"), *player->GetName());
+	
+	//TODO : GC 재생
+	
+	Destroy();
+	
+}
+
+
