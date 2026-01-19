9fce10e1fd18cf47f74152b0b5d52ee025da364f
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-01-19 17:30:26 +0900

    <feat> NetPlayerState 추가

---
+++ Source/RoastStaffGAS/Private/Temp/Net_PlayerState.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Temp/Net_PlayerState.h"
+#include "AbilitySystemComponent.h"
+#include "System/K_LoggingSystem.h"
+#include "Temp/K_NetAttributeSet.h"
+
+ANet_PlayerState::ANet_PlayerState()
+{
+	NetAttributeSet = CreateDefaultSubobject<UK_NetAttributeSet>(TEXT("NetAttributeSet"));
+	if (ASC)
+	{
+		ASC->AddAttributeSetSubobject<UK_NetAttributeSet>(NetAttributeSet);
+		KHS_INFO(TEXT("[NetPlayerState] NetAttributeSet added to ASC"));
+	}
+}
