accb9f0e719b61657df97d43d6a485f0148960d1
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-04 17:16:51 +0900

    <feat> GameplayTags 문서 추가

---
+++ Source/RoastStaffGAS/Private/GAS/Tags/RSGameplayTags.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Tags/RSGameplayTags.h"
+
+
+namespace RSTags
+{
+	//어빌리티 태그
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Projectile, "Ability.Skill.Projectile");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Summon, "Ability.Skill.Summon");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Combat_BasicShoot, "Ability.Combat.BasicShoot");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Combat_BasicShoot_Cooldown, "Ability.Combat.BasicShoot.Cooldown");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Fireball, "Ability.Skill.Fireball");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Skill_Fireball_Cooldown, "Ability.Skill.Fireball.Cooldown");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Movement_Dash, "Ability.Movement.Dash");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Movement_Dash_Cooldown, "Ability.Movement.Dash.Cooldown");
+	
+	//팀 태그
+	UE_DEFINE_GAMEPLAY_TAG(Team_Player, "Team.Player");
+	UE_DEFINE_GAMEPLAY_TAG(Team_Enemy, "Team.Enemy");
+	
+	//상태 태그
+	UE_DEFINE_GAMEPLAY_TAG(State_Dead, "State.Dead");
+	UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Burn, "State.Debuff.Burn");
+	UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Freeze, "State.Debuff.Freeze");
+	UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Shock, "State.Debuff.Shock");
+	UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Slow, "State.Debuff.Slow");
+	UE_DEFINE_GAMEPLAY_TAG(State_Slot_Active, "State.Slot.Active");
+	UE_DEFINE_GAMEPLAY_TAG(State_Debuff_Burning, "State.Debuff.Burning");
+	UE_DEFINE_GAMEPLAY_TAG(State_Dashing, "State.Dashing");
+	
+	//데이터 태그
+	UE_DEFINE_GAMEPLAY_TAG(Data_Damage, "Data.Damage");
+	
+	//GC 태그
+	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Combat_Burn, "GameplayCue.Combat.Burn");
+	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Combat_Fireball_Launch, "GameplayCue.Combat.Fireball.Launch");
+	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Combat_Fireball_Impact, "GameplayCue.Combat.Fireball.Impact");
+	
+	//GE 태그
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_Death, "Event.Combat.Death");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_TakeDamage, "Event.Combat.TakeDamage");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Weapon_Equipped, "Event.Weapon.Equipped");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Weapon_Unequipped, "Event.Weapon.Unequipped");
+	UE_DEFINE_GAMEPLAY_TAG(Event_LevelUp, "Event.LevelUp");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Montage_HitCheck, "Event.Montage.HitCheck");
+	
+	
+}
