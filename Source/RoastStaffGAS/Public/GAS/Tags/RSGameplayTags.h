631863337222705277f07da1ceac2aba2da658f7
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-05 14:55:32 +0900

    <feat> 태그, csv파일 추가

--- Source/RoastStaffGAS/Public/GAS/Tags/RSGameplayTags.h
+++ Source/RoastStaffGAS/Public/GAS/Tags/RSGameplayTags.h
@@ -117,6 +117,8 @@ namespace RSTags
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Weapon_Equipped);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Weapon_Unequipped);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Weapon_Fire);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Weapon_Fire_Projectile);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Weapon_Fire_Summon);
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_LevelUp);
 	
 	//Hitcheck
