d9ab3338741a4165b80fcdacfb5309705a2ca423
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-06 00:46:38 +0900

    <feat> Enemy 사용 AttributeSet 구현

---
+++ Source/RoastStaffGAS/Private/GAS/Attributes/EnemyAttributeSet.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Attributes/EnemyAttributeSet.h"
+
+UEnemyAttributeSet::UEnemyAttributeSet()
+{
+	// 기본값은 BaseAttributeSet 생성자에서 초기화
+	// 실제값은 DT_Enemy에서 주입.
+}
