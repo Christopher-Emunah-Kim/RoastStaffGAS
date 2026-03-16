53987ff2a296c6b39be9f9f2d0798d1eaef98daa
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-16 17:33:19 +0900

    <feat> Weapon Slot Contatiner UI 추가

---
+++ Source/RoastStaffGAS/Public/UI/WeaponSlotContainerWidget.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/RSBaseWidget.h"
+#include "WeaponSlotContainerWidget.generated.h"
+
+/**
+ * WeaponSlotWidget 컨테이너 HUD UI
+ */
+
+class UWeaponSlotWidget;
+
+UCLASS()
+class ROASTSTAFFGAS_API UWeaponSlotContainerWidget : public URSBaseWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UWeaponSlotWidget* GetSlotWidget(int32 SlotIndex) const;
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWeaponSlotWidget> SlotWidget_0;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWeaponSlotWidget> SlotWidget_1;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWeaponSlotWidget> SlotWidget_2;
+	
+};
