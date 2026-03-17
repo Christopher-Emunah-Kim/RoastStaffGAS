44b38e359c62ca8f9efd6a91822431e537b92c7a
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-17 10:57:55 +0900

    <refact> 플레이어 입력처리 책임 이관(Character->Controller)

--- Source/RoastStaffGAS/Private/Character/Player/RSPlayerController.cpp
+++ Source/RoastStaffGAS/Private/Character/Player/RSPlayerController.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Character/Player/RSPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "RoastStaffGAS.h"
 #include "Subsystems/EquipmentSubsystem.h"
 #include "Subsystems/UIManagerSubsystem.h"
@@ -15,12 +18,22 @@ void ARSPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//IMC 등록
+	UEnhancedInputLocalPlayerSubsystem* Subsys =  ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!Subsys || !IMC)
+	{
+		KHS_WARN(TEXT("EnhancedInput 시스템 로딩 실패 / IMC 에섯 미할당"));
+	}
+	Subsys->AddMappingContext(IMC, 0);
+
+	//슬롯 델리게이트 구독
 	GET_GI_SUBSYSTEM_FROM(UUIManagerSubsystem, UMS, GetGameInstance());
 	GET_GI_SUBSYSTEM_FROM(UEquipmentSubsystem, EquipSys, GetGameInstance());
 	
 	EquipSys->OnSlotUpdatedDel.AddDynamic(this, &ARSPlayerController::OnSlotUpdated);
 	KHS_INFO(TEXT("OnSlotUpdatedDel 구독 완료 "));
 
+	//HUD UI오픈
 	if (HUDWidgetClass)
 	{
 		CachedHUDUI = UMS->OpenUI<URSHUDWidget>(HUDWidgetClass);
@@ -37,6 +50,31 @@ void ARSPlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
 	EquipSys->OnSlotUpdatedDel.RemoveDynamic(this, &ARSPlayerController::OnSlotUpdated);
 	
 	Super::EndPlay(EndPlayReason);
+}
+
+void ARSPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EIC->BindAction(IA_Move,  ETriggerEvent::Triggered, this, &ARSPlayerController::OnMove);
+	EIC->BindAction(IA_MouseAim, ETriggerEvent::Triggered, this, &ARSPlayerController::OnMouseAim);
+	//EIC->BindAction(IA_Dash,  ETriggerEvent::Triggered, this, &ARSPlayerController::OnDash);
+	EIC->BindAction(IA_Attack, ETriggerEvent::Started,   this, &ARSPlayerController::OnShootStart);
+	EIC->BindAction(IA_Slot1, ETriggerEvent::Started, this, &ARSPlayerController::OnSlotActivate, 0);
+	EIC->BindAction(IA_Slot2, ETriggerEvent::Started, this, &ARSPlayerController::OnSlotActivate, 1);
+	EIC->BindAction(IA_Slot3, ETriggerEvent::Started, this, &ARSPlayerController::OnSlotActivate, 2);
+}
+
+void ARSPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	// 쿼터뷰 — 마우스 커서 방향으로 캐릭터 회전 / 마우스 좌표 캐싱
+	if (!HandleMouseAim())
+	{
+		return;
+	}
 }
 
 void ARSPlayerController::OnSlotUpdated(int32 SlotIndex)
@@ -80,4 +118,65 @@ void ARSPlayerController::RefreshSlotUI(int32 SlotIndex)
 	Slot->UpdateSlot(SlotData);
 
 	KHS_INFO(TEXT("Slot %d UI 갱신 — WeaponID: %s"), SlotIndex, SlotData ? *SlotData->EquipData.WeaponID.ToString() : TEXT("null"));
+}
+
+bool ARSPlayerController::HandleMouseAim()
+{
+	APawn* ControlledPawn = GetPawn();
+	if (!ControlledPawn)
+	{
+		KHS_WARN(TEXT("Failed to get Controlled Pawn"));
+		return false;
+	}
+	
+	FHitResult HitResult;
+	GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+
+	if (HitResult.bBlockingHit)
+	{
+		CachedAimLocation = HitResult.Location;
+		const FRotator LookAt =	(HitResult.Location - ControlledPawn->GetActorLocation()).Rotation();
+		AimAngle = LookAt.Yaw;
+		ControlledPawn->SetActorRotation(FRotator(0.f, AimAngle, 0.f));
+	}
+	
+	return true;
+}
+
+void ARSPlayerController::OnMove(const FInputActionValue& Value)
+{
+	const FVector2D Input = Value.Get<FVector2D>();
+	LastMoveInput = Input;
+
+	APawn* ControlledPawn = GetPawn();
+	if (!ControlledPawn)
+	{
+		KHS_WARN(TEXT("Failed to get Controlled Pawn"));
+		return;
+	}
+
+	ControlledPawn->AddMovementInput(FVector::ForwardVector, Input.X);
+	ControlledPawn->AddMovementInput(FVector::RightVector,   Input.Y);
+
+}
+
+void ARSPlayerController::OnMouseAim(const FInputActionValue& Value)
+{
+	SetShowMouseCursor(true);
+	
+	FVector2D inputVector = Value.Get<FVector2D>();
+	AimAngle = FMath::RadiansToDegrees(FMath::Atan2(inputVector.Y, inputVector.X));
+}
+
+void ARSPlayerController::OnShootStart(const FInputActionValue& Value)
+{
+	GET_GI_SUBSYSTEM_FROM(UEquipmentSubsystem, EquipSys, GetGameInstance());
+	EquipSys->RequestManualFire(CachedAimLocation);
+}
+
+void ARSPlayerController::OnSlotActivate(const FInputActionValue& Value, int32 SlotIndex)
+{
+	GET_GI_SUBSYSTEM_FROM(UEquipmentSubsystem, EquipSys, GetGameInstance());
+	EquipSys->RequestSlotActivate(SlotIndex);
+
 }
