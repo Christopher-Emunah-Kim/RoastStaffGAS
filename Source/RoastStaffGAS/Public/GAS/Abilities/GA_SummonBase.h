bd5361e287346618ea5ada287c8ed1d5ddec0922
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-19 15:17:55 +0900

    <Refact> 소환형 스킬 공통 로직 추출 및 분리

--- Source/RoastStaffGAS/Public/GAS/Abilities/GA_SummonBase.h
+++ Source/RoastStaffGAS/Public/GAS/Abilities/GA_SummonBase.h
@@ -22,21 +22,31 @@ class ROASTSTAFFGAS_API UGA_SummonBase : public UGA_Base
 	
 public:
 	UGA_SummonBase();
-	
+
 protected:
 	virtual void OnAbilityActivated(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
 	//소환 위치 결정(순수가상함수)
 	virtual FVector DetermineSummonLocation() PURE_VIRTUAL(UGA_SummonBase::DetermineSummonLocation, return FVector::ZeroVector;) 
+	//액티브 모드 처리 가상함수
+	virtual void HandleActiveMode();                                                                           
 	
-	//위치에 오브젝트 소환
+private:
+	//위치에 오브젝트 소환 헬퍼
 	void SpawnSummonObject(const FVector& Location);
-	//필요 데이터 조회
+	//데이터 조회/세팅 헬퍼
+	bool LoadSkillData();
 	bool LoadSummonData(FSkillExecutionData& OutExecData, FSkillAttackMoveTypeParamsSummon& OutSummonParam) const;
-	//소환 데이터 세팅
 	bool SetSummonData(FSummonObjectInitData& InitData, FActorSpawnParameters& SpawnParams, TSubclassOf<AActor>& SummonClass);
-	
+	//액티브 모드 체크 헬퍼
+	bool CheckIsActiveSlot() const;  
+	//액티브 모드 입력처리 헬퍼
+	//(UAbilityTask_WaitConfirmCancel)
+	UFUNCTION()
+	void OnConfirm();
+	UFUNCTION()
+	void OnCancel();
 	
 protected:
 	FName CachedSkillID;
