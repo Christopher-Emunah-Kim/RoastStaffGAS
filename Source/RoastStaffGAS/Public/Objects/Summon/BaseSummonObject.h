ccd07229afc9964f33c33784ca892b61d6038d20
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2026-03-19 14:39:55 +0900

    <feat> 소환 오브젝트 베이스 추가.

---
+++ Source/RoastStaffGAS/Public/Objects/Summon/BaseSummonObject.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/RuntimeDataStructs.h"
+#include "GameFramework/Actor.h"
+#include "BaseSummonObject.generated.h"
+
+/**                                                                                                              
+   * ABaseSummonObject                                                                                               
+   * 소환형 스킬 장판 오브젝트.                                                                                    
+   * SummonRadius 내 Enemy에 DamageGE + StatusGE 즉시 적용.                                    
+   * Lifetime 후 자동 소멸.                                                                                        
+   */   
+
+UCLASS()
+class ROASTSTAFFGAS_API ABaseSummonObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseSummonObject();
+	
+	void InitSummon(const FSummonObjectInitData& InInitData);                                          
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void ApplyGameplayEffectToArea(); 
+	
+private:
+	UPROPERTY()                                                                                                  
+	FSummonObjectInitData InitData;                                                                              
+                                                                                                                   
+	bool bInitialized = false; 
+
+};
